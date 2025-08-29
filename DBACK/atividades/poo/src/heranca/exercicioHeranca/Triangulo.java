9efe13fc46956943ef899fc44903aee4f4ef78f6
= <=>
2025-08-28 22:42:10 -0300

    classe traingulo

---
+++ DBACK/atividades/poo/src/heranca/exercicioHeranca/Triangulo.java
@@ -0,0 +1,17 @@
+package heranca.exercicioHeranca;
+
+public class Triangulo extends Figura2D{
+    
+    private String tipo;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    
+    
+}
