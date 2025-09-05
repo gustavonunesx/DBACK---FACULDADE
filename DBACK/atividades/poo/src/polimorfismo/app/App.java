8b60acf2068405ccb1134f699b7c2e352dd46f45
= <=>
2025-09-04 21:21:58 -0300

    exemplos finalizados

--- DBACK/atividades/poo/src/polimorfismo/app/App.java
+++ DBACK/atividades/poo/src/polimorfismo/app/App.java
@@ -8,31 +8,41 @@ public class App {
     public static void main(String[] args) {
         
         Conta conta = new Conta(1010, 300, "Gustavo");
-
+        conta.getSaldo();
         ContaEmpresa contaEmpresa = new ContaEmpresa(
             4010, 
             3000, 
             "Auro", 
             400000);
 
-             //UPCASTING calsse filha virando pai
+            //UPCASTING calsse filha virando pai
 
-            Conta c1 = contaEmpresa;
-            Conta c2 = new ContaEmpresa(
-                2910, 
-                330,
-                "Astolpgho",
-                231);
+        Conta c1 = contaEmpresa;
+        c1.getClass();
+        Conta c2 = new ContaEmpresa(
+            2910, 
+            330,
+            "Astolpgho",
+            231);
 
-            Conta c3 = new ContaPoupanca(
-                532,
-                1112,
-                "Javalesco", 
-                0.05);
+        Conta c3 = new ContaPoupanca(
+            532,
+            1112,
+            "Javalesco", 
+            0.05);
+        c3.getClass();
+        //DOWNCASTING
 
-            //DOWNCASTING
+        //ContaEmpresa ce1 = conta;
+        // ContaEmpresa ce2 = (ContaEmpresa) conta;
+        ContaEmpresa c4 = (ContaEmpresa) c2;
+        c4.getClass();
 
-            //ContaEmpresa ce1 = conta;
+        if(c2 instanceof ContaEmpresa){
+            ContaEmpresa ce4 = (ContaEmpresa) c2;   
+            ce4.getClass();
+        }
+
     }
 
 
