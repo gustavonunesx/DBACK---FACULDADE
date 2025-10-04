faf696a45897a45e645360cd8e66b5da5c2e23d9
= <=>
2025-10-04 15:54:11 -0300

    começo ex4

---
+++ DBACK/atividades/poo/src/aula09/exercicios/ex4-interface/Boleto.java
@@ -0,0 +1,8 @@
+public class Boleto implements Pagavel {
+
+    @Override
+    public void pagar(){
+        
+    }
+    
+}
---
+++ DBACK/atividades/poo/src/aula09/exercicios/ex4-interface/CartaoCredito.java
@@ -0,0 +1,9 @@
+public class CartaoCredito implements Pagavel {
+
+    @Override
+    public void pagar(){
+        System.out.println("Pagando cartao...");
+    }
+    
+
+}
